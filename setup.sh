00fef5c7e127ed1d75cfecfa6022ea6280bd345a
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2023-02-10 08:55:44 +0700

    Configure kibana yml

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 echo "Creating volume directories."
+rm -Rf ./data
 mkdir -p data/kibana/data data/kibana/config data/es/data01 data/es/data02
+cp kibana.yml data/kibana/config
+source .env
+sed -i "s~KIBANA_PUBLIC_BASE_URL~${KIBANA_PUBLIC_BASE_URL}~g" data/kibana/config/kibana.yml
