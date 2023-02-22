25e89f222a64c456311ca97631b4a429211c86b4
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2023-02-22 21:55:00 +0700

    Review whole setup. Improve via logstash.

---
+++ Makefile
@@ -0,0 +1,23 @@
+.PHONY: init setup start-es stop-es start-log stop-log
+
+init:
+	cp docker/server/.env.example docker/server/.env
+	cp docker/capture/.env.example docker/capture/.env
+	echo "Please update `docker/server/.env` and `docker/capture/.env` files."
+
+setup: stop-log stop-es
+	./setup.sh
+
+start-es:
+	cd docker/server && docker-compose up -d
+
+stop-es:
+	cd docker/server && docker-compose down
+
+start-log:
+	cd docker/capture && docker-compose up -d
+
+stop-log:
+	cd docker/capture && docker-compose down
+
+stop: stop-log stop-es
--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-DATA_ROOT="./data"
+DATA_ROOT="${PWD}/data"
+
+if [ "${DATA_ROOT}" = "/data" ];
+then
+    echo "Not allowed to work from the root of the drive."
+fi
+
 CREATION_VALUE="Updating"
 
 if [ -d "${DATA_ROOT}" ];
@@ -15,12 +21,32 @@ then
 fi
 
 echo "${CREATION_VALUE} volume directories."
-mkdir -p ${DATA_ROOT}/kibana/data ${DATA_ROOT}/kibana/config ${DATA_ROOT}/es/data01 ${DATA_ROOT}/es/data02
+mkdir -p ${DATA_ROOT}/kibana/data ${DATA_ROOT}/kibana/config \
+    ${DATA_ROOT}/logstash/config ${DATA_ROOT}/es/data01 \
+    ${DATA_ROOT}/es/data02 ${DATA_ROOT}/filebeat/config 
+
+pushd docker/server > /dev/null
+source .env
 
 echo "Updating the kibana configuration."
-source .env
-cp kibana.yml ${DATA_ROOT}/kibana/config
+cp ../conf/kibana.yml ${DATA_ROOT}/kibana/config
 sed -i "s~KIBANA_PUBLIC_BASE_URL~${KIBANA_PUBLIC_BASE_URL}~g" ${DATA_ROOT}/kibana/config/kibana.yml
+
+popd > /dev/null
+pushd docker/capture > /dev/null
+source .env
+
+echo "Updating the logstash configuration."
+cp ../conf/logstash.conf ${DATA_ROOT}/logstash/config
+sed -i "s~ES_PASSWORD~${ES_PASSWORD}~g" ${DATA_ROOT}/logstash/config/logstash.conf
+sed -i "s~ES_HOST~${ES_HOST}~g" ${DATA_ROOT}/logstash/config/logstash.conf
+
+echo "Updating the filebeat configuration."
+cp ../conf/filebeat.yml ${DATA_ROOT}/filebeat/config
+chmod go-w ${DATA_ROOT}/filebeat/config/filebeat.yml
+sudo chown root:root ${DATA_ROOT}/filebeat/config/filebeat.yml
+
+popd > /dev/null
 
 echo "Ensuring VM settings are correct"
 VM_MAX_MAP_COUNT=$(sysctl -n vm.max_map_count 2>&1)
